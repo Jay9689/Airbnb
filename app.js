ec5ab860ff11fe16c51ea761f237ade0d25e58ff
Jay9689 <pawarjay916@gmail.com>
2024-02-12 10:20:10 +0530

    added validations

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const Listing = require('./models/listing.js');
 const path = require('path');
 const methodOverride = require('method-override')
 const ejsMate = require('ejs-mate');
+const wrapAsync = require('./utils/wrapAsync.js')
+const ExpressError = require('./utils/ExpressError.js')
+const { listingSchema } = require('./schema.js')
 
 main().then(() => {
     console.log("connected to data base")
@@ -22,10 +25,21 @@ app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, "/public")))
 
 //listing route
-app.get('/listing', async (req, res) => {
+app.get('/listing', wrapAsync(async (req, res, next) => {
     let allListings = await Listing.find({})
     res.render("./listings/index.ejs", { allListings })
-});
+}))
+
+// validation function
+const validatelisting = (req, res, next) => {
+    let { error } = listingSchema.validate(req.body);
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",")
+        throw new ExpressError('400', errMsg)
+    } else {
+        next();
+    }
+}
 
 // /new
 app.get('/listing/new', (req, res) => {
@@ -33,44 +47,56 @@ app.get('/listing/new', (req, res) => {
 })
 
 // show route
-app.get('/listing/:id', async (req, res) => {
+app.get('/listing/:id', wrapAsync(async (req, res, next) => {
     let { id } = req.params;
     let listing = await Listing.findById(id);
     res.render('./listings/show.ejs', { listing })
-})
+}))
 
 // create route
-app.post("/listing", async (req, res) => {
+app.post("/listing", validatelisting, wrapAsync(async (req, res, next) => {
+
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect('/listing')
-})
+
+}))
 
 // edit route
-app.get('/listing/:id/edit', async (req, res) => {
+app.get('/listing/:id/edit', wrapAsync(async (req, res, next) => {
     let { id } = req.params;
     let listing = await Listing.findById(id);
     res.render('./listings/edit', { listing })
-})
+}))
 
 // update route
-app.put('/listing/:id', async (req, res) => {
+app.put('/listing/:id', validatelisting, wrapAsync(async (req, res, next) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing })
     res.redirect(`/listing/${id}`)
-})
+}))
 
 // delete route
-app.delete('/listing/:id', async (req, res) => {
+app.delete('/listing/:id', wrapAsync(async (req, res, next) => {
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id)
     res.redirect('/listing')
     console.log(deletedListing)
-})
+}))
 
 async function main() {
     await mongoose.connect(Mongo_url)
 }
+
+// Error handler Middleware
+app.all('*', (req, res, next) => {
+    next(new ExpressError(404, `<h3 style="color:#fe424d;"> You Entered URL</h3>`))
+})
+
+app.use((err, req, res, next) => {
+    let { statusCode = 500, message = `<h3 style="color:#fe424d;"> You Entered Wrong URL</h3>` } = err
+    res.status(statusCode).render('error.ejs', { message })
+})
 
 app.get('/', (req, res) => {
     res.send("welcome to the server")
--- models/listing.js
+++ models/listing.js
@@ -11,17 +11,13 @@ const listingSchema = new Schema({
     },
 
     image: {
-        url: {
-            type: String,
-            default: "https://unsplash.com/photos/a-person-swimming-over-a-coral-reef-in-the-ocean-tSwRu3Jh0EM",
-            set: (v) => v === "" ? "https://unsplash.com/photos/a-person-swimming-over-a-coral-reef-in-the-ocean-tSwRu3Jh0EM" : v,
-            required: true,
-        },
-        filename: {
-            type: String,
-            required: true,
-        }
-
+        type: String,
+        default:
+            "https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+        set: (v) =>
+            v === ""
+                ? "https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+                : v,
     },
     price: {
         type: Number
