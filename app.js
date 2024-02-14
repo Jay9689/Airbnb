67ed93e2285fc8eb6aee2dbe06ddb377db160511
Jay9689 <pawarjay916@gmail.com>
2024-02-14 22:17:23 +0530

    implemented required corrections

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const Mongo_url = "mongodb://127.0.0.1:27017/wonderlust";
-const Listing = require('./models/listing.js');
 const path = require('path');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
-const wrapAsync = require('./utils/wrapAsync.js');
 const ExpressError = require('./utils/ExpressError.js');
-const { listingSchema, reviewSchema } = require('./schema.js');
-const Review = require('./models/review.js');
+const session = require('express-session')
+const flash = require('connect-flash');
 
-const listings = require('./routes/listing.js'); // Renamed to listingRoutes for clarity
+
+const listings = require('./routes/listing.js'); // Renamed
+const reviews = require('./routes/review.js');
+
+
+const Mongo_url = "mongodb://127.0.0.1:27017/wonderlust";
 
 main().then(() => {
     console.log("connected to database");
@@ -19,7 +21,10 @@ main().then(() => {
     console.log(err);
 });
 
-app.use("/listings", listings); // Changed to "/listings"
+async function main() {
+    await mongoose.connect(Mongo_url);
+}
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
@@ -28,42 +33,37 @@ app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
 
-// validate Review function
-const validateReview = (req, res, next) => {
-    let { error } = reviewSchema.validate(req.body);
-    if (error) {
-        let errMsg = error.details.map((el) => el.message).join(",");
-        throw new ExpressError(400, errMsg);
-    } else {
-        next();
+// express-session
+const sessionOptions = {
+    secret: "mysupersecretcode",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpsOnly: true
     }
-};
-
-// Reviews
-// Reviews Post route
-app.post("/listings/:id/reviews", validateReview, wrapAsync(async (req, res) => {
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review); // Fixed variable name typo
-
-    listing.reviews.push(newReview);
-    await newReview.save();
-    await listing.save();
-    res.redirect(`/listings/${listing._id}`); // Changed to "/listings"
-}));
-
-// Reviews
-// Reviews Delete route
-app.delete('/listings/:id/reviews/:reviewId', wrapAsync(async (req, res) => {
-    let { id, reviewId } = req.params;
-
-    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/listings/${id}`); // Changed to "/listings"
-}));
-
-async function main() {
-    await mongoose.connect(Mongo_url);
 }
+
+// root route
+app.get('/', (req, res) => {
+    res.send("Welcome to the server");
+});
+
+// Express sessions and Flash
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash("success")
+    res.locals.error = req.flash("error")
+    next();
+})
+
+
+// routes
+app.use("/listings", listings);
+app.use("/listings/:id/reviews", reviews);
 
 // Error handler Middleware
 app.all('*', (req, res, next) => {
@@ -73,10 +73,6 @@ app.all('*', (req, res, next) => {
 app.use((err, req, res, next) => {
     let { statusCode = 500, message = `You entered the wrong URL` } = err;
     res.status(statusCode).render('error.ejs', { message });
-});
-
-app.get('/', (req, res) => {
-    res.send("Welcome to the server");
 });
 
 app.listen(3000, () => {
--- routes/listing.js
+++ routes/listing.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const app = express();
 const router = express.Router();
 const wrapAsync = require('../utils/wrapAsync.js');
 const ExpressError = require('../utils/ExpressError.js');
-const { listingSchema, reviewSchema } = require('../schema.js');
+const { listingSchema } = require('../schema.js');
 const Listing = require('../models/listing.js');
 
 
@@ -34,16 +33,21 @@ router.get('/new', (req, res) => {
 // Show route
 router.get('/:id', wrapAsync(async (req, res, next) => {
     let { id } = req.params;
-    console.log(req.params.id)
+    // console.log(req.params.id)
     let listing = await Listing.findById(id).populate('reviews');
+    if (!listing) {
+        req.flash("error", "Listing you requested for does not exist!")
+        res.redirect('/listings')
+    }
     res.render('listings/show', { listing }); // Corrected path
 }));
 
 // Create route
 router.post("/", validateListing, wrapAsync(async (req, res, next) => {
-    console.log(req.body)
-    const newListing = new Listing(req.body.listings)
+    // console.log(req.body)
+    const newListing = new Listing(req.body.listing)
     await newListing.save();
+    req.flash("success", "New Listing Created!")
     res.redirect('/listings'); // Corrected path
 }));
 
@@ -51,14 +55,19 @@ router.post("/", validateListing, wrapAsync(async (req, res, next) => {
 router.get('/:id/edit', wrapAsync(async (req, res, next) => {
     let { id } = req.params;
     let listing = await Listing.findById(id);
+    if (!listing) {
+        req.flash("error", "Listing you requested for does not exist!")
+        res.redirect('/listings')
+    }
     res.render('listings/edit', { listing }); // Corrected path
 }));
 
 // Update route
 router.put('/:id', validateListing, wrapAsync(async (req, res, next) => {
     let { id } = req.params;
-    console.log(req.params.id)
+    // console.log(req.params.id)
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    req.flash("success", "Listing Updated!")
     res.redirect(`/listings/${id}`); // Corrected path
 }));
 
@@ -67,6 +76,7 @@ router.delete('/:id', wrapAsync(async (req, res, next) => {
     console.log(req.params.id)
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
+    req.flash("success", "Listing Deleted!")
     res.redirect('/listings'); // Corrected path
     console.log(deletedListing);
 }));
